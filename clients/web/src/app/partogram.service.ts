a3c07c466cdda22947a4ebf0c1439a9c870415a1
Curt Koster <Curt.Koster@infor.com>
2018-04-16 21:57:22 -0400

    more partogram logic

--- clients/web/src/app/partogram.service.ts
+++ clients/web/src/app/partogram.service.ts
@@ -65,7 +65,7 @@ export class PartogramService {
     const signer = this.signer();
 
     const p: Partogram = {
-      partogram_id: 'fake',
+      partogram_id: '',
       labor_start_time: (Date.now())/1000,
     };
 
